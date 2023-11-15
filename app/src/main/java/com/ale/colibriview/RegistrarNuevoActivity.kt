52118531db80fa9adc6e4883d248499b6a9209ba
AleLV10 <145232629+AleLV10@users.noreply.github.com>
2023-11-14 18:05:24 -0600

    Registrar

--- app/src/main/java/com/ale/colibriview/RegistrarNuevoActivity.kt
+++ app/src/main/java/com/ale/colibriview/RegistrarNuevoActivity.kt
@@ -3,27 +3,33 @@ package com.ale.colibriview
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import com.ale.colibriview.databinding.ActivityRegistrarNuevoBinding
 
 class RegistrarNuevoActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegistrarNuevoBinding
+
+    //VARIABLES DE LOS DATOS QUE VAMOS A REGISTRAR
+    private var name: String = ""
+    private var email: String = ""
+    private var password: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegistrarNuevoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.ingresar.setOnClickListener {
+
+        binding.iniciar.setOnClickListener {
             // Do something in response to button click
             val intent = Intent(this, Inicio::class.java)
             startActivity(intent)
             finish()
         }
 
-        binding.olvida.setOnClickListener {
+        binding.crear.setOnClickListener {
             // Do something in response to button click
-            val intent = Intent(this, restablecer::class.java)
-            startActivity(intent)
-            finish()
+            name=binding.name.toString()
         }
     }
 }
