394256b4703d809c6e6931cd19d6e3d67eef1087
AleLV10 <145232629+AleLV10@users.noreply.github.com>
2023-11-24 21:16:11 -0600

    Tiene un error pero es normal, no genera conflictos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation("com.google.firebase:firebase-auth-ktx:22.2.0")
     implementation("com.google.android.gms:play-services-auth:20.7.0")
     implementation("com.google.firebase:firebase-functions-ktx:20.4.0")
+    implementation("com.google.firebase:firebase-firestore:24.9.1")
 
 
     testImplementation("junit:junit:4.13.2")
