5647f10bfed99c3d6d0ea893e6d3fb9f900a69d6
AleLV10 <145232629+AleLV10@users.noreply.github.com>
2023-11-30 20:23:54 -0600

    O

--- app/src/main/java/com/ale/colibriview/Adapter/OptionAdapter.kt
+++ app/src/main/java/com/ale/colibriview/Adapter/OptionAdapter.kt
@@ -4,23 +4,27 @@ import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.ale.colibriview.PlantillaPreguntaActivity
 import com.ale.colibriview.R
 import com.ale.colibriview.databinding.ItemQuestionBinding
 import com.ale.colibriview.models.Question
 
-class OptionAdapter(private var context: Context, private val question: Question):
+class OptionAdapter(private var context: Context, private val question: Question,private val listener: PlantillaPreguntaActivity):
     RecyclerView.Adapter<OptionAdapter.ViewHolder>() {
 
     private var options : List<String> = listOf(question.Option1,question.Option2,question.Option3)
     private var boolean=false
+    lateinit var AnswerQuestion:String
     inner class ViewHolder(view:View):RecyclerView.ViewHolder(view)
     {
         val binding=ItemQuestionBinding.bind(view)
         var optionView=binding.botonRespuesta
+
         fun setListener(question: Question, valor:Int){
             binding.root.setOnClickListener {
-
+                listener.onClick(question,valor)
             }
 
         }
@@ -37,16 +41,17 @@ class OptionAdapter(private var context: Context, private val question: Question
     override fun getItemCount() :Int =options.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-
         with(holder)
         {
 
             optionView.text=options[position]
-
+            setListener(question, position)
             itemView.setOnClickListener{
-                setListener(question, position)
+
                 optionView.text=options[position]
                 question.UserAnswer=options[position]
+                Toast.makeText(context,"Respuesta en Holder"+question.UserAnswer, Toast.LENGTH_SHORT).show()
+                setAnswerQuestion(question.UserAnswer)
 
                 notifyDataSetChanged()
                 boolean = if (boolean) {
@@ -55,6 +60,7 @@ class OptionAdapter(private var context: Context, private val question: Question
                     itemView.setBackgroundColor(Color.MAGENTA)
                     true
                 }
+
 
                // itemView.setBackgroundResource(R.color.morado)
                 //binding.botonRespuesta.setBackgroundResource(R.color.morado)
@@ -73,8 +79,16 @@ class OptionAdapter(private var context: Context, private val question: Question
             }
            // itemView.setBackgroundResource(R.color.morado)
         }
+    }
+    fun setAnswerQuestion(respuesta:String): String {
+        AnswerQuestion=respuesta
+        Toast.makeText(context,"Respuesta en Set"+question.UserAnswer, Toast.LENGTH_SHORT).show()
+        Toast.makeText(context,"Respuesta en Set2"+AnswerQuestion, Toast.LENGTH_SHORT).show()
+        return AnswerQuestion
+    }
 
-
+    fun getAnswerQ() : String {
+        return AnswerQuestion
     }
 
 }
